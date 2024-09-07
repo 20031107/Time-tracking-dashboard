41134b36ed705eb7fa00efa26a348d37075aed8d
“joesthomas” <joesthomas2003@gmail.com>
2024-09-07 22:43:00 +0100

    little changes

--- time.js
+++ time.js
@@ -18,14 +18,14 @@ const careWeek= document.getElementById('careweek');
 
 function forWorkDaily(data) {
     if (data[0].title === 'Work') {
-      workHours.innerText = `${data[0].timeframes.daily.previous}`;
+      workHours.innerText = `${data[0].timeframes.daily.previous}hrs`;
       workWeek.innerText = `Yesterday -${data[0].timeframes.daily.previous} hrs`
     }
   }
   
   function forWorkWeekly(data) {
     if (data[0].title === 'Work') {
-      workHours.innerText = `${data[0].timeframes.weekly.previous}`;
+      workHours.innerText = `${data[0].timeframes.weekly.previous}hrs`;
       workWeek.innerText = ` Last Week -${data[0].timeframes.weekly.previous} hrs`;
     }
   }
@@ -33,7 +33,7 @@ function forWorkDaily(data) {
     
   function forWorkMonthly(data) {
     if (data[0].title === 'Work') {
-      workHours.innerText = `${data[0].timeframes.monthly.previous}`;
+      workHours.innerText = `${data[0].timeframes.monthly.previous}hrs`;
       workWeek.innerText = ` Last Month -${data[0].timeframes.monthly.previous} hrs`;
     }
   }
@@ -42,23 +42,23 @@ function forWorkDaily(data) {
 
   function forPlayDaily(data) {
     if (data[1].title === 'Play') {
-      playHours.innerText = `${data[1].timeframes.daily.previous}`;
-      playWeek.innerText = `Yesterday -${data[1].timeframes.daily.previous} hrs`
+      playHours.innerText = `${data[1].timeframes.daily.previous}hrs`;
+      playWeek.innerText = `Yesterday - ${data[1].timeframes.daily.previous} hrs`
     }
   }
   
   function forPlayWeekly(data) {
     if (data[1].title === 'Play') {
-      playHours.innerText = `${data[1].timeframes.weekly.previous}`;
-      playWeek.innerText = ` Last Week -${data[1].timeframes.weekly.previous} hrs`;
+      playHours.innerText = `${data[1].timeframes.weekly.previous}hrs`;
+      playWeek.innerText = ` Last Week - ${data[1].timeframes.weekly.previous} hrs`;
     }
   }
 
     
   function forPlayMonthly(data) {
     if (data[1].title === 'Play') {
-      playHours.innerText = `${data[1].timeframes.monthly.previous}`;
-      playWeek.innerText = ` Last Month -${data[1].timeframes.monthly.previous} hrs`;
+      playHours.innerText = `${data[1].timeframes.monthly.previous}hrs`;
+      playWeek.innerText = ` Last Month - ${data[1].timeframes.monthly.previous} hrs`;
     }
   }
 
@@ -66,14 +66,14 @@ function forWorkDaily(data) {
 
     function forStudyDaily(data) {
       if (data[2].title === 'Study') {
-        studyHours.innerText = `${data[2].timeframes.daily.previous}`;
-        studyWeek.innerText = `Yesterday -${data[2].timeframes.daily.previous} hr`
+        studyHours.innerText = `${data[2].timeframes.daily.previous}hr`;
+        studyWeek.innerText = `Yesterday - ${data[2].timeframes.daily.previous} hr`
       }
     }
     
     function forStudyWeekly(data) {
       if (data[2].title === 'Study') {
-        studyHours.innerText = `${data[2].timeframes.weekly.previous}`;
+        studyHours.innerText = `${data[2].timeframes.weekly.previous} hrs`;
         studyWeek.innerText = ` Last Week -${data[2].timeframes.weekly.previous} hrs`;
       }
     }
@@ -81,7 +81,7 @@ function forWorkDaily(data) {
       
     function forStudyMonthly(data) {
       if (data[2].title === 'Study') {
-        studyHours.innerText = `${data[2].timeframes.monthly.previous}`;
+        studyHours.innerText = `${data[2].timeframes.monthly.previous} hrs`;
         studyWeek.innerText = ` Last Month -${data[2].timeframes.monthly.previous} hrs`;
       }
     }
@@ -91,14 +91,14 @@ function forWorkDaily(data) {
     
     function forExerciseDaily(data) {
       if (data[3].title === 'Exercise') {
-        exerciseHours.innerText = `${data[3].timeframes.daily.previous}`;
+        exerciseHours.innerText = `${data[3].timeframes.daily.previous}hr`;
         exerciseWeek.innerText = `Yesterday - ${data[3].timeframes.daily.previous} hr`
       }
     }
     
     function forExerciseWeekly(data) {
       if (data[3].title === 'Exercise') {
-        exerciseHours.innerText = `${data[3].timeframes.weekly.previous}`;
+        exerciseHours.innerText = `${data[3].timeframes.weekly.previous}hrs`;
         exerciseWeek.innerText = ` Last Week - ${data[3].timeframes.weekly.previous} hrs`;
       }
     }
@@ -106,7 +106,7 @@ function forWorkDaily(data) {
       
     function forExerciseMonthly(data) {
       if (data[3].title === 'Exercise') {
-        exerciseHours.innerText = `${data[3].timeframes.monthly.previous}`;
+        exerciseHours.innerText = `${data[3].timeframes.monthly.previous}hrs`;
         exerciseWeek.innerText = ` Last Month - ${data[3].timeframes.monthly.previous} hrs`;
       }
     }
@@ -116,14 +116,14 @@ function forWorkDaily(data) {
       
     function forSocialDaily(data) {
       if (data[4].title === 'Social') {
-        socialHours.innerText = `${data[4].timeframes.daily.previous}`;
+        socialHours.innerText = `${data[4].timeframes.daily.previous}hrs`;
         socialWeek.innerText = `Yesterday - ${data[4].timeframes.daily.previous} hrs`
       }
     }
     
     function forSocialWeekly(data) {
       if (data[4].title === 'Social') {
-        socialHours.innerText = `${data[4].timeframes.weekly.previous}`;
+        socialHours.innerText = `${data[4].timeframes.weekly.previous}hrs`;
         socialWeek.innerText = ` Last Week - ${data[4].timeframes.weekly.previous} hrs`;
       }
     }
@@ -131,21 +131,21 @@ function forWorkDaily(data) {
       
     function forSocialMonthly(data) {
       if (data[4].title === 'Social') {
-        socialHours.innerText = `${data[4].timeframes.monthly.previous}`;
+        socialHours.innerText = `${data[4].timeframes.monthly.previous}hrs`;
         socialWeek.innerText = ` Last Month - ${data[4].timeframes.monthly.previous} hrs`;
       }
     }
 
     function forCareDaily(data) {
       if (data[5].title === 'Self Care') {
-        careHours.innerText = `${data[5].timeframes.daily.previous}`;
+        careHours.innerText = `${data[5].timeframes.daily.previous}hr`;
         careWeek.innerText = `Yesterday - ${data[5].timeframes.daily.previous} hr`
       }
     }
     
     function forCareWeekly(data) {
       if (data[5].title === 'Self Care') {
-        careHours.innerText = `${data[5].timeframes.weekly.previous}`;
+        careHours.innerText = `${data[5].timeframes.weekly.previous}hrs`;
         careWeek.innerText = ` Last Week - ${data[5].timeframes.weekly.previous} hrs`;
       }
     }
@@ -153,7 +153,7 @@ function forWorkDaily(data) {
       
     function forCareMonthly(data) {
       if (data[5].title === 'Self Care') {
-        careHours.innerText = `${data[5].timeframes.monthly.previous}`;
+        careHours.innerText = `${data[5].timeframes.monthly.previous}hrs`;
         careWeek.innerText = ` Last Month - ${data[5].timeframes.monthly.previous} hrs`;
       }
     }
